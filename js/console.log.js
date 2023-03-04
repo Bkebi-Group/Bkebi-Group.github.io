7b4965fb03ea544a441a6c1ea987bb24ea81541e
Bnq Dzj <64721413+BNQDZJ@users.noreply.github.com>
2023-03-04 19:02:34 +0800

    Print the total downloads in the console and automatically follow the version number (#25)
    * Update console.log.js
    
    * Update console.log.js

--- js/console.log.js
+++ js/console.log.js
@@ -1,8 +1,17 @@
-console.log("%c+",
-`font-size: 1px;
-padding: 100px 70px;
-background-image: url(https://bkebi-group.github.io/img/logo.svg);
-background-size: contain;
-background-repeat: no-repeat;
-color: transparent;`);
-console.log(`%c Bkebi GC %c V1.0.3R %c https://github.com/Bkebi-Group/Bkebi-GC-Release/releases/latest`, "color: #fff; background: #5f5f5f", "color: #fff; background: #4bc729", "")
+const owner = "Bkebi-Group";
+const repo = "Bkebi-GC-Release";
+fetch(`https://api.github.com/repos/${owner}/${repo}/releases/latest`)
+  .then(response => response.json()) 
+  .then(data => {
+    const tag_name = data.tag_name;
+    console.log(`%c Bkebi GC %c ${tag_name} %c https://github.com/Bkebi-Group/Bkebi-GC-Release/releases/latest`, "color: #fff; background: #5f5f5f", "color: #fff; background: #4bc729", "")
+     });
+
+let url = "https://img.shields.io/github/downloads/Bkebi-Group/Bkebi-GC-Release/total.json";
+
+fetch(url)
+  .then(response => response.json())
+  .then(data => {
+    const downloads_value = data.value;
+    console.log(`%c Total Downloads %c ${downloads_value} %c `, "color: #fff; background: #5f5f5f", "color: #fff; background: #4bc729", "")
+  })
